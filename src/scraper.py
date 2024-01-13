6da6cdc17db0217600699f3563b6f4b23705c005
karinavagner112 <wagner_97@mail.ru>
2024-01-13 16:04:17 +0100

    add scraper functions

--- src/scraper.py
+++ src/scraper.py
@@ -1,53 +1,82 @@
-# coding=utf-8
-
 import requests
 from bs4 import BeautifulSoup
-
-# scraping logic 
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-}
-
-hauptseite = 'https://www.hwr-berlin.de/hwr-berlin/fachbereiche-und-bps/fb-2-duales-studium/'
-
-generalPage = requests.get(hauptseite,headers=headers)
-soupGeneral = BeautifulSoup(generalPage.text, 'html.parser')
-
-main_content = soupGeneral.find_all('div', class_= 'grid-group stop') #content ist in diesen div's
-
-for div_element in main_content:
-    headings = div_element.find_all(['h1', 'h2', 'h3'])
-    for heading in headings:
-        print("ueberschrift:" + heading.text)
-        next_wysiwyg = heading.find_next('div', class_='wysiwyg')
-        if next_wysiwyg:
-            text = next_wysiwyg.get_text(strip=True)
-            print("Text: " + text)        
-            print('-' * 50)
+import json
 
 
+def get_header_level(element):
+    tag_name = element.name
+    header_level = 0
 
-def download_pdfs(url): 
-    pdf = []
+    if tag_name and tag_name.startswith("h"):
+        header_level = int(tag_name[1])
 
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    return header_level
 
-    main = soup.find('main')
-    links = main.find_all('a', href=lambda href: (href and href.endswith('.pdf')))
 
-    i = 0
+def build_content(html_elements):
+    content = []
+    prev_header_level = 999
 
-    for link in links: 
-        link_http = 'http://www.hwr-berlin.de' + link.get('href')
-        print(link_http)
-        response = requests.get(link_http, headers=headers)
+    for element in html_elements:
+        if get_header_level(element) != 0:
+            break
+        content.append(element.get_text(strip=True))
 
-        # Write content in pdf file
-        pdf = open("pdf"+str(i)+".pdf", 'wb')
-        pdf.write(response.content)
-        pdf.close()
-        print("File ", i, " downloaded")
+    for element in html_elements:
+        header_level = get_header_level(element)
+        if header_level <= prev_header_level and header_level > 0:
+            if header_level < prev_header_level:
+                prev_header_level = header_level
+            header = {}
+            header["title"] = element.get_text(strip=True)
+            header["content"] = build_content(get_children(html_elements, element))
+            content.append(header)
 
-print("All PDF files downloaded")
+    return content
+
+
+def get_children(html_elements, header_element):
+    children = []
+
+    main_header_level = get_header_level(header_element)
+
+    record = False
+
+    for element in html_elements:
+        if header_element == element:
+            record = True
+            continue
+        if record:
+            header_level = get_header_level(element)
+            if header_level <= main_header_level and header_level != 0:
+                break
+            children.append(element)
+
+    return children
+
+
+def main():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+    }
+
+    hauptseite = (
+        "https://www.hwr-berlin.de/hwr-berlin/fachbereiche-und-bps/fb-2-duales-studium/"
+    )
+
+    generalPage = requests.get(hauptseite, headers=headers)
+    soupGeneral = BeautifulSoup(generalPage.text, "html.parser")
+    main_content = soupGeneral.find("main")
+    content_with_tags = main_content.find_all(
+        ["h1", "h2", "h3", "h4", "h5", "h6", "p", "a", "link"], text=True
+    )
+
+    page = []
+
+    page = build_content(content_with_tags)
+
+    print(page)
+
+
+if __name__ == "__main__":
+    main()
