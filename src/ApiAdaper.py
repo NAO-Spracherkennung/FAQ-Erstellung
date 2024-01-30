73187b733b07887c9e5a448de7e7708e6cf9eabe
Denis <d-kasakow@mail.de>
2024-01-30 14:41:22 +0100

    placeholders für ApiAdapter erstellt

---
+++ src/ApiAdaper.py
@@ -0,0 +1,91 @@
+class Adapter:
+    def __init__(self):
+        pass
+
+    def createFAQ(self, text):
+        self.preprocessInput()
+        self.callAPI()
+        self.preparePrompt()
+        self.postprocessOutput()
+
+    def preprocessInput(self):
+        pass
+
+    def preparePrompt(self):
+        pass
+
+    def callAPI(self):
+        pass
+
+    def postprocessOutput(self):
+        pass
+
+
+class Jurassic2Chat:
+    def __init__(self):
+        pass
+
+
+class Jurassic2Complete:
+    def __init__(self):
+        pass
+
+
+class CohereCommand:
+    def __init__(self):
+        pass
+
+
+class GeminiPro:
+    def __init__(self):
+        pass
+
+
+class Palm2:
+    def __init__(self):
+        pass
+
+
+class GPT_Neo:
+    def __init__(self):
+        pass
+
+
+class GPT_J:
+    def __init__(self):
+        pass
+
+
+class Fairseq:
+    def __init__(self):
+        pass
+
+
+class GPT_NeoX:
+    def __init__(self):
+        pass
+
+
+class Llama2:
+    def __init__(self):
+        pass
+
+
+class Tii_Falcon:
+    def __init__(self):
+        pass
+
+
+class GPT35_Turbo:
+    def __init__(self):
+        pass
+
+
+class GPT4:
+    def __init__(self):
+        pass
+
+
+class GPT4_Turbo:
+    def __init__(self):
+        pass
