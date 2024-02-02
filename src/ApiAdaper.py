e9adf503d89ab864f2d573d15ceb190c42d2e3cd
Denis <91327614+denis-kasak@users.noreply.github.com>
2024-02-02 13:11:01 +0100

    API Doku Links zu Adapter.py hinzugefügt

--- src/ApiAdaper.py
+++ src/ApiAdaper.py
@@ -22,70 +22,112 @@ class Adapter:
 
 
 class Jurassic2Chat:
+    """
+    https://docs.ai21.com/reference/j2-chat-api
+    """
     def __init__(self):
         pass
 
 
 class Jurassic2Complete:
+    """
+    https://docs.ai21.com/reference/j2-complete-api-ref
+    """
     def __init__(self):
         pass
 
 
 class CohereCommand:
+    """
+    https://docs.cohere.com/reference/generate
+    """
     def __init__(self):
         pass
 
 
 class GeminiPro:
+    """
+    https://cloud.google.com/vertex-ai/docs/generative-ai/pricing?hl=de
+    """
     def __init__(self):
         pass
 
 
 class Palm2:
+    """
+    https://cloud.google.com/vertex-ai/docs/generative-ai/pricing?hl=de
+    """
     def __init__(self):
         pass
 
 
 class GPT_Neo:
+    """
+    https://goose.ai/docs/api
+    """
     def __init__(self):
         pass
 
 
 class GPT_J:
+    """
+    https://goose.ai/docs/api
+    """
     def __init__(self):
         pass
 
 
 class Fairseq:
+    """
+    https://goose.ai/docs/api
+    """
     def __init__(self):
         pass
 
 
 class GPT_NeoX:
+    """
+    https://goose.ai/docs/api
+    """
     def __init__(self):
         pass
 
 
 class Llama2:
+    """
+    https://cloud.google.com/model-garden?hl=de
+    """
     def __init__(self):
         pass
 
 
 class Tii_Falcon:
+    """
+    https://cloud.google.com/model-garden?hl=de
+    """
     def __init__(self):
         pass
 
 
 class GPT35_Turbo:
+    """
+    https://platform.openai.com/docs/introduction
+    """
     def __init__(self):
         pass
 
 
 class GPT4:
+    """
+    https://platform.openai.com/docs/introduction
+    """
     def __init__(self):
         pass
 
 
 class GPT4_Turbo:
+    """
+    https://platform.openai.com/docs/introduction
+    """
     def __init__(self):
         pass
