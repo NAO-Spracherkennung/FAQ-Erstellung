91034c1e3b8a3fc744d499402ecad9b2967a2152
karinavagner112 <wagner_97@mail.ru>
2024-02-04 22:19:01 +0100

    add pdf parser

--- src/pdfParser.py
+++ src/pdfParser.py
@@ -1,6 +1,8 @@
 import requests
 import fitz
 import tempfile
+import os
+from bs4 import BeautifulSoup
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -20,6 +22,45 @@ def download_pdf(url):
         return response.content
     else:
         return None
+    
+def download_pdfs_with_label(url, target_label):
+    """
+    Extracts the content from a pdf file.
+    Returns an array with page content.
+
+    """
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return None
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    main_tag = soup.find("main")
+
+    if main_tag:
+        all_a_tags = main_tag.find_all("a", href=True)
+
+        pdf_links = [
+            a["href"]
+            for a in all_a_tags
+            if a["href"].endswith(".pdf") and a.text.strip() == target_label
+        ]
+
+        pdf_contents = []
+
+        for pdf_link in pdf_links:
+            link_http = "http://www.hwr-berlin.de" + pdf_link
+            pdf_content = download_pdf(link_http)
+            if pdf_content:
+                os.makedirs("data", exist_ok=True)
+                path = os.path.join("data", f"{target_label}.pdf")
+                with open(path, "wb") as file:
+                    file.write(pdf_content)
+                pdf_contents.append(pdf_content)
+
+        return pdf_contents
+    else:
+        print("Der <main>-Tag wurde nicht gefunden.")
+        return None
 
 
 def extract_text_from_pdf(pdf_file):
@@ -35,28 +76,32 @@ def extract_text_from_pdf(pdf_file):
 
         pdf_path = temp_file.name
 
-        doc = fitz.open(pdf_path)
-        page_count = len(doc)
-        text_content = []
-        
-        for page_number in range(page_count):
-            page = doc[page_number]
-            text_content.append(page.getText())
+        with fitz.open(pdf_path) as doc:
+            page_count = len(doc)
+            text_content = []
 
-        doc.close()
+            for page_number in range(page_count):
+                page = doc[page_number]
+                text_content.append(page.get_text())
         return text_content
     else:
         return None
 
 
 def main():
+    url = "https://www.hwr-berlin.de/studium/studiengaenge/detail/61-informatik/"
 
-    url = "https://www.hwr-berlin.de/fileadmin/portal/Dokumente/HWR-Berlin/Mitteilungsbl%C3%A4tter/2023/Mitteilungsblatt_11-2023_ZHV_Konsoliderte_Fassung_nach_f%C3%BCnfter_%C3%84nderung_RStud-Pr%C3%BCfO_d_e.pdf"
-    pdf = download_pdf(url)
-
-    pdf_text_content = extract_text_from_pdf(pdf)
-
-    print(pdf_text_content)
+    target_labels = [
+        "Modulübersicht",
+        "Rahmenstudien- und Prüfungsordnung der HWR Berlin",
+    ]
+    pdf_text_content = []
+    for target_label in target_labels:
+        pdf_contents = download_pdfs_with_label(url, target_label)
+        for pdf_content in pdf_contents:
+            pdf_text_content = extract_text_from_pdf(pdf_content)
+            print("PDF")
+        print(pdf_text_content)
 
 
 if __name__ == "__main__":
