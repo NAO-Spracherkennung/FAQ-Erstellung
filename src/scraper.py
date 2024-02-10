be684fa17c3cff617c44b56270e68a750dcd08d9
karinavagner112 <wagner_97@mail.ru>
2024-02-10 20:47:15 +0100

    add functions for updating questions

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,8 @@
 import requests
+import json
+import os
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
 def get_header_level(element):
@@ -87,34 +90,51 @@ def scrape_to_json(url):
     )
 
     page = []
-
     page = build_content(content_with_tags)
 
     return page
 
 
-def main():
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-    }
+def get_last_segment_from_url(url):
+    """
+    Extracts the last section from the URL to be used as a filename later
 
-    hauptseite = (
-        "https://www.hwr-berlin.de/hwr-berlin/fachbereiche-und-bps/fb-2-duales-studium/"
-    )
-
-    response = requests.get(hauptseite, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    main_content = soup.find("main")
-    content_with_tags = main_content.find_all(
-        ["h1", "h2", "h3", "h4", "h5", "h6", "p", "a", "link"], string=True
-    )
-
-    page = []
-
-    page = build_content(content_with_tags)
-
-    print(page)
+    """
+    parsed_url = urlparse(url)
+    path_segments = parsed_url.path.split("/")
+    last_segment = path_segments[-1]
+    return last_segment
 
 
-if __name__ == "__main__":
-    main()
+def save_json(text, target_label):
+    """
+    Saves extracted text as a json file
+
+    """
+    os.makedirs("json", exist_ok=True)
+    path = os.path.join("json", f"{target_label}.json")
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(text, file, ensure_ascii=False, indent=4)
+
+
+def compare_json(folder_path, label, new_json):
+    """
+    Checks whether json object already exists and compares the old and new json objects
+    Returns True if json objects are equal
+    Returns False if json objects are not equal or or there was no json object yet
+
+    """
+    current_directory = os.getcwd()
+    full_path = os.path.join(current_directory, folder_path)
+    file_path = os.path.join(full_path, label + ".json")
+
+    if os.path.exists(file_path):
+        with open(file_path, "r") as file:
+            existing_json_obj = json.load(file)
+
+        if existing_json_obj == new_json:
+            return True
+        else:
+            return False
+    else:
+        return False
