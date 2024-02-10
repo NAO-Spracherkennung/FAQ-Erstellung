ec209bb32d001dd85a21018dcd95110e8f75b9e5
karinavagner112 <wagner_97@mail.ru>
2024-02-10 20:47:45 +0100

    add main funktion

--- src/main.py
+++ src/main.py
@@ -94,8 +94,34 @@ def create_faq(source_text):
     return all_faq
 
 
+def main():
+    urls = [
+        "https://www.hwr-berlin.de/studium/studiengaenge/detail/61-informatik",
+        "https://www.hwr-berlin.de/hwr-berlin/fachbereiche-und-bps/fb-2-duales-studium",
+        "https://www.hwr-berlin.de/hwr-berlin/fachbereiche-und-bps/fb-2-duales-studium/studieren-am-fachbereich/einschreibung-und-rueckmeldung",
+        "https://www.hwr-berlin.de/studium/bewerbung/bewerbung-duales-studium/bewerbung-fb-duales-studium",
+        "https://www.hwr-berlin.de/hwr-berlin/fachbereiche-und-bps/fb-2-duales-studium/duales-studium-im-profil"
+    ]
+    data_exist = os.path.isdir("data")
+    json_exist = os.path.isdir("json")
+    
+    """Website"""
+    if json_exist: 
+        for url in urls: 
+            label = scraper.get_last_segment_from_url(url)
+            new_text = scraper.scrape_to_json(url) 
+            compare = scraper.compare_json("json", label, new_text)
+            print(compare)
+            if not compare: 
+                #werden weiter fragen erstellt
+                faq = create_faq(new_text)
+    else:
+        for url in urls: 
+            source_text = scraper.scrape_to_json(url)
+            label = scraper.get_last_segment_from_url(url)
+            scraper.save_json(source_text,label)
+            faq = create_faq(source_text)
+
+
 if __name__ == "__main__":
-    source_text = scraper.scrape_to_json(
-        "https://www.hwr-berlin.de/studium/studiengaenge/detail/61-informatik/"
-    )
-    faq = create_faq(source_text)
+    main()
